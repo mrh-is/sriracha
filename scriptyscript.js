9842b2f109645c4dd7399b0e849391e6eaa64198
Michael Helmbrecht <michael.helmbrecht@gmail.com>
2013-01-22 19:18:29 -0500

    Awesome Pokemon test image

--- scriptyscript.js
+++ scriptyscript.js
@@ -6,4 +6,10 @@ var gameInit = function() {
 	game.page = game.canvas.getContext('2d'); // game.page === game.ctx
 	game.w = game.canvas.width;
 	game.h = game.canvas.height;
+	var img = new Image();
+	img.src = "New-Pokemon-Drinking-Game.png";
+	img.onload = function(){
+		console.log("img load");
+		game.page.drawImage(img, 0, 0);
+	};
 }
