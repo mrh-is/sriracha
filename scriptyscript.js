1db445a7b113df9815f66e51f2a3b064f2a65aa0
Michelle Lew <lew.michelle@gmail.com>
2013-01-22 19:15:10 -0500

    init forrealz seriously

--- scriptyscript.js
+++ scriptyscript.js
@@ -1,4 +1,7 @@
 var gameInit = function() {
+
+	var game = {};
+	
 	game.canvas = document.getElementById('gameCanvas');
 	game.page = game.canvas.getContext('2d'); // game.page === game.ctx
 	game.w = game.canvas.width;
