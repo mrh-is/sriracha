b40e180c499068ce3193e1bf820b39757216abcc
Michael Helmbrecht <michael.helmbrecht@gmail.com>
2013-01-22 19:27:41 -0500

    create Level constructor

--- scriptyscript.js
+++ scriptyscript.js
@@ -9,7 +9,16 @@ var gameInit = function() {
 	var img = new Image();
 	img.src = "New-Pokemon-Drinking-Game.png";
 	img.onload = function(){
-		console.log("img load");
 		game.page.drawImage(img, 0, 0);
 	};
+}
+
+function Level(level) {
+	this.level = level;
+	// create Schloo with given rate
+	this.schloo = new Schloo(1);
+	// create Bowl with given radius
+	this.bowl = new Bowl(10);
+	// create Bottle with given radius
+	this.bottle = new Bottle(1);
 }
