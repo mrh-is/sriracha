fa0eb43a1a380da4a38d23ef05a80f9e3ffddfe5
Michelle Lew <lew.michelle@gmail.com>
2013-01-23 21:34:16 -0500

    change background in schloo.js

--- schloo.js
+++ schloo.js
@@ -2,7 +2,7 @@ function Schloo() { // constructor for background / schloo
 
 	this.draw = function() {
 		var bgImage = new Image();
-		bgImage.src = "New-Pokemon-Drinking-Game.png";
+		bgImage.src = "table.png";
 
 		bgImage.onload = function() {
 			game.ctx.drawImage(bgImage, 0, 0);
