90f733236941516401784436f6a0e803af09b497
jenn tran <j6tran@gmail.com>
2013-01-28 16:34:22 -0500

    time bar working, with color change
    time bar with color

--- schloo.js
+++ schloo.js
@@ -1,28 +1,41 @@
 function Schloo(level, refreshInterval) { // constructor for background / schloo
 
+
 	this.width = 200;
 
 	
-	var percentSlepy;
-	var intervalDuration = 250;
-	
-		
-	function getHungreadOnly(duration){
-		var increment = intervalDuration*this.width/duration;
-	}
-	
-	function resetStart(){
-		
-	}
-	
 	// progress bar is redrawn!
-	this.hungerBar = function(percent) {
-		game.ctx.strokeStyle = "black";
+	this.hungerBar = function(percentHungry) {
+		game.ctx.save();
+		game.ctx.beginPath();
+		//game.ctx.strokeStyle = "black";
 		game.ctx.strokeRect(680, 25, this.width, 25);
 		game.ctx.beginPath();
-		game.ctx.fillRect(680, 25, percent*this.width, 25);
 		
+		if (percentHungry > 0.90){
+			game.ctx.fillStyle = "red";
+				}
+		else if (0.50 < percentHungry) {
+			game.ctx.fillStyle = "yellow";
+		}
+		else {
+			game.ctx.fillStyle = "blue";
+		}
+		
+		game.ctx.fillRect(680, 25, percentHungry*this.width, 25);
+		game.ctx.closePath();
+		game.ctx.restore();
 	}	
+	
+	this.myScore = function(){
+		game.ctx.save();
+		game.ctx.font= "16px Arial";
+		//game.ctx.fillStyle = "black";
+		game.ctx.fillText("don't let schloo get hungry!", 650, 75);
+		game.ctx.restore();
+	}
+
+	
 
 	this.totalTime = 500;  // 10,000 milliseconds
 	this.elapsedTime = 0;
@@ -30,7 +43,7 @@ function Schloo(level, refreshInterval) { // constructor for background / schloo
 	this.refreshInterval = refreshInterval;
 
 
-	this.draw = function(percent) {
+	this.draw = function(percentHungry) {
 		var bgImage = new Image(), _self = this;
 		bgImage.src = "table.png";
 
@@ -42,14 +55,24 @@ function Schloo(level, refreshInterval) { // constructor for background / schloo
 			var percent = schloo.elapsedTime/schloo.totalTime;
 			if (percent < 1){  // only execute if percent is less than 100%
 				_self.hungerBar(percent);
+				game.ctx.font= "16px Arial";
+				game.ctx.fillText(parseInt(percent * 100)+"%", 849, 75);
 			}
 			else  // only execute if percent is 100% or more
 			{
-				_self.hungerBar(1);
-				
+				_self.hungerBar(1);	game.ctx.font= "16px Arial";
+				game.ctx.fillText("100%", 845,75);
 			}
+			_self.myScore();
 		};
 	};
+	
+/*
+	this.keepScore = function (){
+		print(parseInt(percentHungry*this.width)+"%");
+	}
+*/
+
 	
 	
 }
