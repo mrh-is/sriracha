4ae9ebb36906f11b7b7bf640176dac0ac5625a44
Michelle Lew <lew.michelle@gmail.com>
2013-01-29 00:28:55 -0500

    cosmetic changes to % bar and canvas position

--- schloo.js
+++ schloo.js
@@ -29,9 +29,9 @@ function Schloo(level, refreshInterval) { // constructor for background / schloo
 	
 	this.myScore = function(){
 		game.ctx.save();
-		game.ctx.font= "16px Arial";
+		game.ctx.font= "14px Arial";
 		//game.ctx.fillStyle = "black";
-		game.ctx.fillText("don't let schloo get hungry!", 650, 75);
+		game.ctx.fillText("don't let schloo get hungry!!", 694, 100);
 		game.ctx.restore();
 	};
 
@@ -62,14 +62,17 @@ function Schloo(level, refreshInterval) { // constructor for background / schloo
 			if (percent < 1) {  // only execute if percent is less than 100%
 				_self.hungerBar(percent);
 				game.ctx.font= "16px Arial";
-				game.ctx.fillText(Math.round(percent * 100)+"%", 849, 75);
+				game.ctx.fillText(Math.round(percent * 100)+"%", 699, 75);
+				game.ctx.fillText(" HUNGER LEVEL", 732, 75);
 			}
 			else  // only execute if percent is 100% or more
 			{
 				_self.hungerBar(1);	game.ctx.font= "16px Arial";
-				game.ctx.fillText("100%", 845,75);
+				game.ctx.fillText("100%", 700,75);
 			}
-			_self.myScore();
+			if(percent > 0.5) {
+				_self.myScore();
+			}
 		};
 	};
 	
