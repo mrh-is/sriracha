90b674968c06eb2842e9aa6194ab3a1ae18fc195
Michelle Lew <lew.michelle@gmail.com>
2013-01-29 02:37:48 -0500

    start game with intro screen and instructions

---
+++ message.js
@@ -0,0 +1,10 @@
+function Message() { // constructor for background / schloo
+
+
+	game.ctx.save();
+	ctx.fillStyle = "rgb(0, 0, 255)";
+	ctx.globalAlpha = 0.5;
+	ctx.fillRect(100, 100, 700, 400);
+	game.ctx.restore();
+	
+}
--- scriptyscript.js
+++ scriptyscript.js
@@ -1,13 +1,11 @@
 var game = {}; // global
 var keypresses = [0, 0, 0, 0]; // track state of keypresses: up down left right
+var introStatus = true;
 
 var gameInit = function() {
 
 	game.canvas = document.getElementById("gameCanvas");
 	game.ctx = game.canvas.getContext("2d"); // game context
-
-	game.overlay = document.getElementById("overlay");
-	game.overlayCtx = game.overlay.getContext("2d"); // game context
 
 	// makes canvas the area of focus for keyboard
 	game.canvas.setAttribute("tabindex","0");
@@ -59,14 +57,26 @@ var gameInit = function() {
 			keypresses[3] = 0;
 		}
 		if(event.keyCode === 32) { // spacebar
-			level.bottle.open = false;
-			console.log("bottle closed!");
+			if(introStatus === true) {
+				startRefresh();
+				introStatus = false;
+			}
+			else {
+				level.bottle.open = false;
+				console.log("bottle closed!");
+			}
 		}
 	};
 
 	game.canvas.addEventListener("keydown", game.onKeyDown, false); // why does this need to be defined after the function is defined?
 	game.canvas.addEventListener("keyup", game.onKeyUp, false);
 
+	var gameStart = new Intro();
+	console.log("started game");
+
+};
+
+function startRefresh() {
 	var refreshInterval = 50;  // 50 millisecond
 	var level = new Level(1, refreshInterval);
 	console.log("Level is created!");
@@ -80,15 +90,70 @@ var gameInit = function() {
 	};
 
 	window.setInterval(game.update, refreshInterval);
-};
-
+}
 
 function Level(level, refreshInterval) {
 	this.level = level;
+	// this.message = new Message();
 	// create Schloo with given rate
 	this.schloo = new Schloo(1, refreshInterval);
 	// create Bowl with given radius
 	this.bowl = new Bowl(50,150,200);
 	// create Bottle with given radius
 	this.bottle = new Bottle(200,200,20);
+}
+
+function Intro() {
+	game.canvas = document.getElementById("gameCanvas");
+	game.ctx = game.canvas.getContext("2d"); // game context
+	game.ctx.save();
+	game.ctx.fillStyle = "rgba(60, 60, 60, 1)";
+	game.ctx.fillRect(0, 0, 900, 600);
+	game.ctx.restore();
+	game.ctx.save();
+	game.ctx.fillStyle = "rgba(255, 255, 255, 0.9)";
+	game.ctx.fillRect(50, 50, 800, 500);
+	game.ctx.restore();
+
+	game.ctx.save();
+	game.ctx.font= "70px Trebuchet MS";
+	game.ctx.fillStyle = "#D23B27";
+	game.ctx.fillText("get that", 375, 145);
+	game.ctx.restore();
+
+	game.ctx.save();
+	game.ctx.font= "115px Trebuchet MS";
+	game.ctx.fillStyle = "#D23B27";
+	game.ctx.fillText("sriracha", 220, 240);
+	game.ctx.restore();
+
+	game.ctx.save();
+	game.ctx.font= "205px Trebuchet MS";
+	game.ctx.fillStyle = "#D23B27";
+	game.ctx.fillText("!", 630, 243);
+	game.ctx.restore();
+
+	game.ctx.save();
+	game.ctx.font= "18px Trebuchet MS";
+	game.ctx.fillStyle = "#D23B27";
+	game.ctx.fillText("You guys!!!", 220, 315);
+	game.ctx.fillText("Michelle is HUNGRY. The problem is, she's allergic to any", 220, 350);
+	game.ctx.fillText("food that doesn't have sriracha on it. Help her out by", 220, 370);
+	game.ctx.fillText("covering the entire bowl before she gets too hungry.", 220, 390);
+	game.ctx.fillText("Use the ARROW KEYS for navigation", 220, 450);
+	game.ctx.fillText("and the SPACE BAR to dispense the sriracha.", 220, 470);
+	game.ctx.restore();
+
+	game.ctx.save();
+	game.ctx.font= "14px Trebuchet MS";
+	game.ctx.fillStyle = "#D23B27";
+	game.ctx.fillText("HIT THE SPACE BAR TO START.", 350, 525);
+
+	game.ctx.save();
+	game.ctx.beginPath();
+	game.ctx.strokeStyle = "#D23B27";
+	game.ctx.lineWidth = 2;
+	game.ctx.strokeRect(180, 420, 540, 70);
+	game.ctx.closePath();
+	game.ctx.restore();
 }
